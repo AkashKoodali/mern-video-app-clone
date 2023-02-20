41e3b152c575b014d3927ff5bd2d178dfe403b6a
Akash Koodali <akashkoodali9@gmail.com>
2023-02-20 15:59:38 +0530

    Created backend server

---
+++ server/index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/users.routes.js";
+import videoRoutes from "./routes/video.routes.js";
+import commentRoutes from "./routes/comments.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
+
+
+const app = express();
+dotenv.config();
+
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+      console.log("Connected to DB");
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+//middlewares
+app.use(cookieParser())
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
+
+//error handler
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong!";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+});
+
+app.listen(4000, () => {
+  connect();
+  console.log("Connected to Server");
+});
